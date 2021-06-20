e9dbce2cb2231acd4fd5c3e9884491cb34799cce
Aryan Raj <araj60988@gmail.com>
2021-06-20 11:32:11 +0530

    comited final project

---
+++ src/components/pages/Services.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../App.css";
+import Footer from "../Footer";
+export default function services() {
+  return (
+    <>
+      <h1 className="services">SERVICES</h1>
+      <Footer />
+    </>
+  );
+}
